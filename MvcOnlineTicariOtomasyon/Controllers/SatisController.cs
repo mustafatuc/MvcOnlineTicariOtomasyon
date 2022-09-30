55eda96d8dc1f96d8f30b3abd9f522aceade88ce
mustafatuc <mustafatuc1@gmail.com>
2022-09-30 15:15:46 +0300

    Add project files.

---
+++ MvcOnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.SatisHarekets.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAd,
+                                               Value = x.Urunid.ToString()
+                                           }
+                                         ).ToList();
+            ViewBag.dgr1 = deger1;
+            List<SelectListItem> deger2 = (from x in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CariAd+" "+x.CariSoyad,
+                                               Value = x.Cariid.ToString()
+                                           }
+                                       ).ToList();
+            ViewBag.dgr2 = deger2;
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAd + " " + x.PersonelSoyad,
+                                               Value = x.Personelid.ToString()
+                                           }
+                                    ).ToList();
+            ViewBag.dgr3 = deger3;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket s)
+        {s.Tarih=DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.SatisHarekets.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int Cariid { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(100,ErrorMessage = "En Fazla 100 Karekter Yazabilirsiniz")]
+        public string CariAd { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(100)]
+        [Required(ErrorMessage ="Bu alanı Boş Geçemessiniz!")]
+        public string CariSoyad { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string  CariSehir { get; set; }
+        [Column(TypeName = "Varchar")]
+        [StringLength(100)]
+        public string CariMail { get; set; }
+        public bool Durum { get; set; }
+        public ICollection<SatisHareket> SatisHarekets { get; set; }
+
+
+    }
+}
